4162beae8e9d0969b251ea6952f32463c52cdcb3
Johan Yim <johanjyyim@gmail.com>
2018-10-06 00:57:50 +0800

    Inserted a few lines of code

--- app/src/main/java/Interviewd/MainActivity.java
+++ app/src/main/java/Interviewd/MainActivity.java
@@ -2,18 +2,71 @@ package Interviewd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button[] actionButtons = new Button[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupAll();
+        UpdateAll();
     }
 
-    private void make(){
-        
+
+    //============================================================================================== Methods
+
+    public void startRecording(){
+
     }
+
+
+    //============================================================================================== Setup
+
+    private void setupAll(){
+        setupActionButtons();
+        setupRecordingButton();
+    }
+
+    private void setupActionButtons(){
+        actionButtons[0] = findViewById(R.id.homeButton);
+        actionButtons[1] = findViewById(R.id.scriptButton);
+        actionButtons[2] = findViewById(R.id.moreButton);
+        actionButtons[3] = findViewById(R.id.settingsButton);
+
+
+    }
+
+    private void setupRecordingButton(){
+        final Button recButton = findViewById(R.id.recordButton);
+        recButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Interviewd", "Now recording");
+                for(Button button : actionButtons){
+                    button.setVisibility(View.INVISIBLE);
+                }
+
+                recButton.setText("Now Recording");
+
+            }
+        });
+    }
+
+    //============================================================================================== Update
+
+    private void UpdateAll(){
+
+    }
+
+
+
+
 
 }
