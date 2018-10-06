55ee9a254f3b5cd698119cc5fd5f1184b8cc2703
Johan Yim <johanjyyim@gmail.com>
2018-10-06 13:20:49 +0800

    Big changes to the Main GUI

--- app/src/main/java/Interviewd/MainActivity.java
+++ app/src/main/java/Interviewd/MainActivity.java
@@ -1,15 +1,16 @@
 package Interviewd;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener,Tab4.OnFragmentInteractionListener {
 
-    Button[] actionButtons = new Button[4];
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,42 +23,71 @@ public class MainActivity extends AppCompatActivity {
 
     //============================================================================================== Methods
 
-    public void startRecording(){
 
-    }
 
 
     //============================================================================================== Setup
 
     private void setupAll(){
+        setupTabs();
         setupActionButtons();
-        setupRecordingButton();
     }
 
-    private void setupActionButtons(){
-        actionButtons[0] = findViewById(R.id.homeButton);
-        actionButtons[1] = findViewById(R.id.scriptButton);
-        actionButtons[2] = findViewById(R.id.moreButton);
-        actionButtons[3] = findViewById(R.id.settingsButton);
+    private void setupTabs(){
+
+        int[] tabImageIds = {
+                R.drawable.home,
+                R.drawable.script,
+                R.drawable.more,
+                R.drawable.settings
+        };
+
+        String[] tabNames = {
+                getResources().getString(R.string.tab1_name),
+                getResources().getString(R.string.tab2_name),
+                getResources().getString(R.string.tab3_name),
+                getResources().getString(R.string.tab4_name)
+        };
 
 
-    }
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
 
-    private void setupRecordingButton(){
-        final Button recButton = findViewById(R.id.recordButton);
-        recButton.setOnClickListener(new View.OnClickListener() {
+        for(int i = 0; i < tabNames.length; i++){
+            tabLayout.addTab(tabLayout.newTab().setText(tabNames[i]));
+            tabLayout.getTabAt(i).setIcon(tabImageIds[i]);
+        }
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Log.d("Interviewd", "Now recording");
-                for(Button button : actionButtons){
-                    button.setVisibility(View.INVISIBLE);
-                }
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
 
-                recButton.setText("Now Recording");
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
+
     }
+
+    private void setupActionButtons(){
+
+    }
+
+
 
     //============================================================================================== Update
 
@@ -66,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-
-
+    }
 }
